cbd0484848c2afb1638779c4402f2ee3f333bc7d
turnertrowbridge <ttrowbridge7322@sdsu.edu>
2022-12-23 23:54:29 -0800

    Configured for PyMySQL

--- lambda/add_workout.py
+++ lambda/add_workout.py
@@ -1,42 +1,43 @@
-from __future__ import print_function
+import sys
+import logging
+import rds_config
+import pymysql
 
-import json
-import uuid
-import decimal
-import os
-import boto3
+#rds settings
+rds_host  = "rds-instance-endpoint"
+name = rds_config.db_username
+password = rds_config.db_password
+db_name = rds_config.db_name
 
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
 
-# Helper class to convert a DynamoDB item to JSON.
-class DecimalEncoder(json.JSONEncoder):
-    def default(self, o):
-        if isinstance(o, decimal.Decimal):
-            if o % 1 > 0:
-                return float(o)
-            else:
-                return int(o)
-        return super(DecimalEncoder, self).default(o)
+try:
+    conn = pymysql.connect(host=rds_host, user=name, passwd=password, db=db_name, connect_timeout=5)
+except pymysql.MySQLError as e:
+    logger.error("ERROR: Unexpected error: Could not connect to MySQL instance.")
+    logger.error(e)
+    sys.exit()
 
+logger.info("SUCCESS: Connection to RDS MySQL instance succeeded")
+def handler(event, context):
+    """
+    This function fetches content from MySQL RDS instance
+    """
 
-# Get the service resource.
-dynamodb = boto3.resource('dynamodb')
+    item_count = 0
 
-# set environment variable
-TABLE_NAME = os.environ['TABLE_NAME']
+    with conn.cursor() as cur:
+        cur.execute("create table Employee ( EmpID  int NOT NULL, Name varchar(255) NOT NULL, PRIMARY KEY (EmpID))")
+        cur.execute('insert into Employee (EmpID, Name) values(1, "Joe")')
+        cur.execute('insert into Employee (EmpID, Name) values(2, "Bob")')
+        cur.execute('insert into Employee (EmpID, Name) values(3, "Mary")')
+        conn.commit()
+        cur.execute("select * from Employee")
+        for row in cur:
+            item_count += 1
+            logger.info(row)
+            #print(row)
+    conn.commit()
 
-
-def lambda_handler(event, context):
-    table = dynamodb.Table(TABLE_NAME)
-    # put item in table
-    response = table.put_item(
-        Item={
-            'id': str(uuid.uuid4())
-        }
-    )
-
-    print("PutItem succeeded:")
-    print(json.dumps(response, indent=4, cls=DecimalEncoder))
-
-    return {
-        'statusCode': 200,
-    }
+    return "Added %d items from RDS MySQL table" %(item_count)
