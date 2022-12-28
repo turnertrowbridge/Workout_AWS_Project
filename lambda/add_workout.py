f04fd3f1ded25268130996a076f6aaa7e452afe7
turnertrowbridge <ttrowbridge7322@sdsu.edu>
2022-12-28 15:46:08 -0800

    Removed old stuff

--- lambda/add_workout.py
+++ lambda/add_workout.py
@@ -5,37 +5,6 @@ import pymysql
 import boto3
 from botocore.exceptions import ClientError
 
-
-
-# db_username = "ttrow99"
-# db_name = "WorkoutTrackingTable"
-# db_endpoint = "workoutprojectstack-workouttrackingtable087385ee-befffhnoqsx8.cpzmyn3b5mc4.us-west-2.rds.amazonaws.com"
-# region_name = "us-west-2"
-# db_password = "r-SPM0tGh-osymdqTGL-mhA=f=0,^H"
-
-# def get_secret():
-#
-#
-#     # Create a Secrets Manager client
-#     session = boto3.session.Session()
-#     client = session.client(
-#         service_name='secretsmanager',
-#         region_name=rds_config.region_name
-#     )
-#
-#     try:
-#         get_secret_value_response = client.get_secret_value(
-#             SecretId=rds_config.secret_name
-#         )
-#     except ClientError as e:
-#         # For a list of exceptions thrown, see
-#         # https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
-#         raise e
-#
-#     # Decrypts secret using the associated KMS key.
-#     return get_secret_value_response['SecretString']
-
-
 # rds settings
 db_endpoint = rds_config.db_endpoint
 db_username = rds_config.db_username
@@ -57,13 +26,13 @@ logger.info("SUCCESS: Connection to RDS MySQL instance succeeded")
 
 def lambda_handler(event, context):
 
-    This function fetches content from MySQL RDS instance
-    """
+    #This function fetches content from MySQL RDS instance
 
-    # s3 = boto3.client('s3')
-    # data = s3.get_object(Bucket='workoutprojectstack-testbucketworkout12327b145c6-s0isntjv74xa', Key='test_input.txt')
-    # contents = data['Body'].read()
-    # print(contents)
+
+    s3 = boto3.client('s3')
+    data = s3.get_object(Bucket='workoutprojectstack-testbucketworkout12327b145c6-s0isntjv74xa', Key='test_input.txt')
+    contents = data['Body'].read()
+    print(contents)
 
     item_count = 0
 
