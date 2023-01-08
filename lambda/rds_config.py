7c3af9b126ba744d24615b013f5b2496c8a474dd
turnertrowbridge <turnertrowbridge@icloud.com>
2023-01-07 16:29:53 -0800

    migrated old files to new

---
+++ lambda/rds_config.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+#config file containing credentials for rds mysql instance
+db_username = "ttrow99"
+db_name = "workoutprojectstack-workouttrackingtable087385ee-tl9ubjiaioda"
+db_endpoint = "workoutprojectstack-workouttrackingtable087385ee-tl9ubjiaioda.cpzmyn3b5mc4.us-west-2.rds.amazonaws.com"
+secret_name = "WorkoutProjectStackWorkoutT-YVNiRSKhEPY1"
+region_name = "us-west-2"
+db_password = "rceqloBu=V5TEU2V3D==wi^vlKdr9d"
