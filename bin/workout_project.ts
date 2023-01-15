77b1825d4086879aa167d47d9e01996138f44941
turnertrowbridge <ttrowbridge7322@sdsu.edu>
2023-01-15 11:06:49 -0800

    Env variables set to default

--- bin/workout_project.ts
+++ bin/workout_project.ts
@@ -5,5 +5,6 @@ import { WorkoutProjectStack } from '../lib/workout_project-stack';
 
 const app = new cdk.App();
 new WorkoutProjectStack(app, 'WorkoutProjectStack', {
+  // set up in user region
   env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
 });
